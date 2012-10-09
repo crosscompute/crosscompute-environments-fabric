b67f8481920054884b7307229c30187440535555
Roy Hyunjin Han <rhh@crosscompute.com>
2012-10-08 21:40:55 -0400

    Updated to node 0.8.11

--- fabfile.py
+++ fabfile.py
@@ -143,7 +143,7 @@ def install_geospatial():
 @task
 def install_node():
     def customize(repositoryPath):
-        run('git checkout b88c3902b241cf934e75443b934f2033ad3915b1') # v0.8.9
+        run('git checkout e1f39468fa580c1e4cb15fac621f87944ee625dc') # v0.8.11
     install_library('https://github.com/joyent/node.git', yum_install='openssl-devel', customize=customize)
     with virtualenv():
         run('npm install -g commander expresso http-proxy node-inspector should socket.io')
