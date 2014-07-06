55c6e03400f3dd0feb6af48a4e3a4e901ea9ad57
Roy Hyunjin Han <rhh@crosscompute.com>
2014-07-06 08:29:01 -0700

    Restore python-devel

--- fabfile.py
+++ fabfile.py
@@ -123,7 +123,7 @@ def install_base():
     }
     with settings(warn_only=True):
         sudo("sed -i 's/enabled=0/enabled=1/' /etc/yum.repos.d/epel.repo")
-    sudo('yum -y install vim-minimal tmux git deltarpm python27-devel')
+    sudo('yum -y install vim-minimal tmux git deltarpm python-devel python27-devel')
     sudo('yum -y install wget tar unzip which virtualenvwrapper')
     with settings(warn_only=True):
         sudo('rm -rf /usr/lib/python2.7/site-packages/setuptools*')
